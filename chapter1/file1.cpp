92069ca225259ce331dd7bfbac8cccf048c537a8
pitiwat <pl1425@ic.ac.uk>
2025-12-01 15:09:16 +0000

    feat:add

---
+++ chapter1/file1.cpp
@@ -0,0 +1,12 @@
+// Comments
+// Import libraries
+#include <iostream>
+
+// main function
+int main() {
+
+    // Print Hello World on screen
+    std::cout << "Hello world" << std::endl; // end of the line endl // std : standard
+
+    return 0;
+}
